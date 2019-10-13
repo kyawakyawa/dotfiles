181f1e7284a406c3d44f2d961d7a2bca031e2dfa
kyawakyawa <kyawashell@gmail.com>
2019-10-13 22:52:39 +0900

    add rep

--- .vim/template/cpp.cpp
+++ .vim/template/cpp.cpp
@@ -10,6 +10,8 @@ int dx[]={1, -1, 0, 0, 0};
 #define RFOR(i,a,b) for (int i=(b)-1;i>=(a);i--)
 #define REP(i,n) for (int i=0;i<(n);i++)
 #define RREP(i,n) for (int i=(n)-1;i>=0;i--)
+#define rep(i,n) for (int i=0;i<(n);i++)
+#define rrep(i,n) for (int i=(n)-1;i>=0;i--)
 
 int main(){
 
