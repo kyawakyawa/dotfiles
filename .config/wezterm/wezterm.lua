32e8a83b7ecfa7e9f895bfb28b26f767a3834baf
kyawakyawa <kyawashell@gmail.com>
2022-04-02 17:13:42 +0900

    visual bell(wezterm)

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,19 +1,34 @@
 local wezterm = require 'wezterm';
 
 return {
+  -- Font
+  font_size = 10.0,
   font = wezterm.font_with_fallback({
     "Delugia Nerd Font",
     "HackGenNerd",
   }),
+  -- IME
   use_ime = true,
-  font_size = 10.0,
+  -- Colorscheme etc
   color_scheme = "Solarized Dark Higher Contrast",
+  window_background_opacity=0.8,
+  -- Key
+  keys = {
+    {key="F11", mods="CTRL|SHIFT", action="ToggleFullScreen"}, -- Ctrl + Shift + F!! でフルスクリーンのon/offができる
+  },
+  -- Other
   hide_tab_bar_if_only_one_tab = true,
   adjust_window_size_when_changing_font_size = false,
   exit_behavior = "Close",
   warn_about_missing_glyphs=false, -- フォントグリフが見つからないときの警告を出さない
-  window_background_opacity=0.8,
-  keys = {
-    {key="F11", mods="CTRL|SHIFT", action="ToggleFullScreen"}, -- Ctrl + Shift + F!! でフルスクリーンのon/offができる
-  }
+  visual_bell = {
+    fade_in_function = "EaseIn",
+    fade_in_duration_ms = 15,
+    fade_out_function = "EaseOut",
+    fade_out_duration_ms = 15,
+  },
+  audible_bell = "Disabled",
+  colors = {
+    visual_bell = "#202020"
+  },
 }
