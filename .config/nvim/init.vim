15fd6efaac6704ccf4d47dace134ecc7570a7634
kyawakyawa <kyawashell@gmail.com>
2022-04-08 00:42:06 +0900

    nvim-cmp

--- .config/nvim/init.vim
+++ .config/nvim/init.vim
@@ -18,6 +18,7 @@ let g:dein_dir = expand('~/.config/nvim')
 let s:vim_denops_toml = g:dein_dir . '/vim-denops.toml'
 "let s:ddc_toml = g:dein_dir . '/ddc.toml'
 let s:ddc_nvim_lsp_toml = g:dein_dir . '/ddc-nvim-lsp.toml'
+"let s:nvim_cmp_lsp_toml = g:dein_dir . '/nvim-cmp-lsp.toml'
 let s:git_toml = g:dein_dir . '/git.toml'
 let s:vim_airline_toml = g:dein_dir . '/vim-airline.toml'
 let s:fzf_toml = g:dein_dir . '/fzf.toml'
@@ -31,6 +32,7 @@ let s:markdown_toml = g:dein_dir . '/markdown.toml'
 call dein#load_toml(s:vim_denops_toml, {'lazy': 1})
 "call dein#load_toml(s:ddc_toml, {'lazy': 1})
 call dein#load_toml(s:ddc_nvim_lsp_toml, {'lazy': 1})
+"call dein#load_toml(s:nvim_cmp_lsp_toml, {'lazy': 1})
 call dein#load_toml(s:git_toml, {'lazy': 0})
 call dein#load_toml(s:vim_airline_toml, {'lazy': 0})
 call dein#load_toml(s:fzf_toml, {'lazy': 1})
---
+++ .config/nvim/lua/nvim_cmp_cfg/init.lua
@@ -0,0 +1,177 @@
+-- Setup nvim-cmp.
+local cmp = require'cmp'
+
+cmp.setup({
+  snippet = {
+    -- REQUIRED - you must specify a snippet engine
+    expand = function(args)
+      vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+      -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+      -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+      -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+    end,
+  },
+  mapping = {
+    ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
+    ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
+    ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
+    ['<C-y>'] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
+    ['<C-e>'] = cmp.mapping({
+      i = cmp.mapping.abort(),
+      c = cmp.mapping.close(),
+    }),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+  },
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    { name = 'vsnip' }, -- For vsnip users.
+    -- { name = 'luasnip' }, -- For luasnip users.
+    -- { name = 'ultisnips' }, -- For ultisnips users.
+    -- { name = 'snippy' }, -- For snippy users.
+  }, {
+    { name = 'buffer' },
+  })
+})
+
+-- Set configuration for specific filetype.
+cmp.setup.filetype('gitcommit', {
+  sources = cmp.config.sources({
+    { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
+  }, {
+    { name = 'buffer' },
+  })
+})
+
+-- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline('/', {
+  sources = {
+    { name = 'buffer' }
+  }
+})
+
+-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline(':', {
+  sources = cmp.config.sources({
+    { name = 'path' }
+  }, {
+    { name = 'cmdline' }
+  })
+})
+
+-- nvim lsp
+local nvim_lsp = require('lspconfig')
+local util = require('lspconfig/util')
+
+-- Use an on_attach function to only map the following keys
+-- after the language server attaches to the current buffer
+local on_attach = function(client, bufnr)
+  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+  local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+
+  -- Enable completion triggered by <c-x><c-o>
+  buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+  -- Mappings.
+  local opts = { noremap=true, silent=true }
+
+  -- See `:help vim.lsp.*` for documentation on any of the below functions
+  buf_set_keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+  buf_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+  buf_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+  buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+  buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+  buf_set_keymap('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
+  buf_set_keymap('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
+  buf_set_keymap('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+  buf_set_keymap('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+  buf_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+  buf_set_keymap('n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+  buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+  buf_set_keymap('n', '<space>E', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
+  buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
+  buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
+  buf_set_keymap('n', '<space>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
+  buf_set_keymap('n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
+
+end
+
+-- Set capabilities
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+
+capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+
+-- Set up language servers
+
+---- ccls
+nvim_lsp['ccls'].setup {
+  on_attach = on_attach,
+  flags = {
+    debounce_text_changes = 150,
+  },
+  init_options = {
+    cache = {
+      directory = "/tmp/ccls-cache";
+    },
+  },
+  capabilities = capabilities,
+  filetypes = { 'c', 'cpp', 'objc', 'objcpp', 'cuda' },
+  root_dir = function(fname)
+    return util.root_pattern('compile_commands.json', '.ccls', 'compile_flags.txt')(fname)
+      or util.path.dirname(fname)
+  end,
+}
+
+---- clangd (OpenCL)
+nvim_lsp['clangd'].setup {
+  on_attach = on_attach,
+  cmd = {
+    "clangd",
+    "--background-index",
+    "--header-insertion=never",
+    "--pch-storage=memory",
+    "--clang-tidy"
+  },
+  capabilities = capabilities,
+  filetypes = { 'cl' },
+  root_dir = function(fname)
+    return util.root_pattern('compile_commands.json', '.cache', 'compile_flags.txt')(fname)
+      or util.path.dirname(fname)
+  end,
+}
+
+---- cmake-language-server
+nvim_lsp['cmake'].setup{
+  on_attach = on_attach,
+  capabilities = capabilities,
+}
+
+---- efm-langserver
+nvim_lsp['efm'].setup{
+  on_attach = on_attach,
+  filetypes = {'python'},
+}
+
+---- typescript language server
+nvim_lsp['tsserver'].setup{
+  on_attach = on_attach,
+  capabilities = capabilities,
+}
+
+-- ---- deno language server
+-- nvim_lsp['denols'].setup{
+--   on_attach = on_attach,
+--   capabilities = capabilities,
+--   root_dir = function(fname)
+--     return util.root_pattern('deno.json', 'deno.jsonc')(fname)
+--   end,
+-- }
+
+---- python language server
+nvim_lsp['pyright'].setup{
+  on_attach = on_attach,
+  capabilities = capabilities,
+}
+
+
