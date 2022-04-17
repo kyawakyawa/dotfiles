9c8785778818bcd1b0ce6bdb7b2c2a2dda630e80
kyawakyawa <kyawashell@gmail.com>
2022-04-17 22:14:00 +0900

    dotfilesLink.sh

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -3,5 +3,5 @@
 if [ ! -e ~/.vimrc ]; then ln -s ~/dotfiles/.vimrc ~/.vimrc ; else echo .vimrc is exist; fi
 if [ ! -e ~/.gitconfig ]; then ln -s ~/dotfiles/.gitconfig ~/.gitconfig ; else echo .gitconfig is exist; fi
 if [ ! -e ~/.tmux.conf ]; then ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf ; else echo .tmux.conf is exist; fi
-mkdir -p .config
+mkdir -p ~/.config
 if [ ! -e ~/.config/nvim ]; then ln -s ~/dotfiles/.config/nvim ~/.config/nvim ; else echo .config/nvim is exist; fi
