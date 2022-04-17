fbf4bd045450ea815ffa9a16746f8eb4b3f30f89
kyawakyawa <kyawashell@gmail.com>
2022-04-17 22:15:04 +0900

    fix dotfilesLink*.sh

--- dotfilesLinkEssential.sh
+++ dotfilesLinkEssential.sh
@@ -2,6 +2,7 @@
 if [ ! -e ~/.vimrc ]; then ln -s ~/dotfiles/.vimrc ~/.vimrc ; else echo .vimrc is exist; fi
 if [ ! -e ~/.vim ]; then ln -s ~/dotfiles/.vim ~/.vim ; else echo .vim is exist; fi
 if [ ! -e ~/.gitconfig ]; then ln -s ~/dotfiles/.gitconfig ~/.gitconfig ; else echo .gitconfig is exist; fi
+if [ ! -e ~/.tmux.conf ]; then ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf ; else echo .tmux.conf is exist; fi
+mkdir -p ~/.config
 if [ ! -e ~/.config/nvim ]; then ln -s ~/dotfiles/.config/nvim ~/.config/nvim ; else echo .config/nvim is exist; fi
 if [ ! -e ~/.config/efm-langserver ]; then ln -s ~/dotfiles/.config/efm-langserver ~/.config/efm-langserver ; else echo .config/efm-langserver is exist; fi
-if [ ! -e ~/.config/.tmux.conf ]; then ln -s ~/dotfiles/.config/.tmux.conf ~/.config/.tmux.conf ; else echo .config/.tmux.conf is exist; fi
