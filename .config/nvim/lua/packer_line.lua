0daf37b2e226e47642e501749f3e41a6fc5efd92
kyawakyawa <kyawashell@gmail.com>
2022-09-19 14:11:59 +0900

    lualine's config

--- .config/nvim/lua/packer_line.lua
+++ .config/nvim/lua/packer_line.lua
@@ -3,7 +3,12 @@ local line = {}
 line.setup = function(use)
 
   -- lualine.nvim
-  use 'nvim-lualine/lualine.nvim'
+  use ({
+    'nvim-lualine/lualine.nvim',
+    config = function()
+      require('lualine_cfg')
+    end,
+  })
 
   -- barbar.nvim
   use 'romgrk/barbar.nvim'
