a36b9af727af9efc7e0c04d0626caaa43d91d34c
kyawakyawa <kyawashell@gmail.com>
2022-09-19 11:44:21 +0900

    gitsigns's config

--- .config/nvim/lua/packer_git.lua
+++ .config/nvim/lua/packer_git.lua
@@ -3,7 +3,12 @@ local git = {}
 git.setup = function(use)
 
   -- gitsigns.nvim
-  use 'lewis6991/gitsigns.nvim'
+  use ({
+    'lewis6991/gitsigns.nvim',
+    config = function()
+      require('gitsigns').setup()
+    end,
+  })
 
   -- diffview.nvim
   use 'sindrets/diffview.nvim'
