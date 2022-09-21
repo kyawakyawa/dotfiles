dc6324d300694ff476da944ffb369f3cafa7ca02
kyawakyawa <kyawashell@gmail.com>
2022-09-21 11:39:22 +0900

    setup nvim tree

--- .config/nvim/lua/packer_file_explorer.lua
+++ .config/nvim/lua/packer_file_explorer.lua
@@ -3,7 +3,20 @@ local file_explorer = {}
 file_explorer.setup = function(use)
 
   -- nvim-tree.lua
-  use 'kyazdani42/nvim-tree.lua'
+  use ({
+    'kyazdani42/nvim-tree.lua',
+    config = function()
+      -- disable netrw at the very start of your init.lua (strongly advised)
+      vim.g.loaded = 1
+      vim.g.loaded_netrwPlugin = 1
+
+      -- empty setup using defaults
+      require("nvim-tree").setup()
+
+      local nt_api = require("nvim-tree.api")
+      vim.keymap.set('n', '<space>e', nt_api.tree.toggle, { noremap=true, silent=true })
+    end,
+  })
 
 end
 
