37f15edb6d6c4aeb89e1cd04b1ea0aa2402802eb
kyawakyawa <kyawashell@gmail.com>
2022-10-05 17:07:45 +0900

    fix nvim-autopairs

--- .config/nvim/lua/packer_brackets.lua
+++ .config/nvim/lua/packer_brackets.lua
@@ -5,7 +5,12 @@ brackets.setup = function(use)
   -- nvim-autopairs
   use {
 	  "windwp/nvim-autopairs",
-    config = function() require("nvim-autopairs").setup {} end
+    config = function()
+      require("nvim-autopairs").setup {}
+
+      -- こうしないとC-hをBSの代わりに使うときにうまく動かない
+      vim.api.nvim_set_keymap('i', '<C-h>', '<BS>', { noremap = false })
+    end
   }
 
 end
