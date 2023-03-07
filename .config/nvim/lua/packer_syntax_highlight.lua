f5ae04d7e792b975e19fd8baaa07e375c00e44d9
kyawakyawa <kyawashell@gmail.com>
2023-03-07 17:38:25 +0900

    markdown markdown_inlier(treesitter)

--- .config/nvim/lua/packer_syntax_highlight.lua
+++ .config/nvim/lua/packer_syntax_highlight.lua
@@ -69,6 +69,9 @@ syntax_highlight.setup = function(use)
 
           vim.api.nvim_command('echo "Regex"')
           vim.api.nvim_command('TSInstall regex')
+
+          vim.api.nvim_command('echo "Markdown"')
+          vim.api.nvim_command("TSInstall markdown markdown_inline")
         end,
         vim.api.nvim_command('command MyTSInstall lua my_ts_install()')
       end,
