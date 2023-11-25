f7a787ecbb68e8743471b2efd739f53079e3eead
kyawakyawa <kyawashell@gmail.com>
2023-11-25 18:09:06 +0900

    evim

---
+++ .config/nvim/essential.lua
@@ -0,0 +1,14 @@
+vim.cmd([[
+  set nocompatible
+  filetype plugin on
+  set number
+  syntax enable
+  set expandtab
+  set tabstop=2
+  set shiftwidth=2
+  set cindent
+  set hlsearch
+  set cursorline
+  set smartindent
+  set hidden
+]])
