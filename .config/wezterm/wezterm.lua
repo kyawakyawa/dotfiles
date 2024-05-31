dc64b69437c7b1cc991960379eceae06839f5d5c
kyawakyawa <kyawashell@gmail.com>
2024-05-31 14:30:16 +0900

    update wezterm.lua

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -22,6 +22,38 @@ elseif is_windows() then
   config.default_prog = { "wsl.exe", "--distribution", "Arch", "--cd", "~" }
 end
 
+-- > wezterm.exe start PowerShell でPowerShellを起動できる
+if is_windows() then
+  local launch_menu = {}
+
+  table.insert(launch_menu, {
+    label = 'PowerShell',
+    args = { 'powershell.exe', '-NoLogo' },
+  })
+
+  -- Find installed visual studio version(s) and add their compilation
+  -- environment command prompts to the menu
+  for _, vsvers in
+    ipairs(
+      wezterm.glob('Microsoft Visual Studio/20*', 'C:/Program Files (x86)')
+    )
+  do
+    local year = vsvers:gsub('Microsoft Visual Studio/', '')
+    table.insert(launch_menu, {
+      label = 'x64 Native Tools VS ' .. year,
+      args = {
+        'cmd.exe',
+        '/k',
+        'C:/Program Files (x86)/'
+          .. vsvers
+          .. '/BuildTools/VC/Auxiliary/Build/vcvars64.bat',
+      },
+    })
+  end
+  
+  config.launch_menu = launch_menu
+end
+
 -- Font
 config.font_size = 12.0
 config.font = wezterm.font_with_fallback({
