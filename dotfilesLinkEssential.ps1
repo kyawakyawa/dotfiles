43dbaa9d9d0f6df7bb02f73c317572356c898597
kyawakyawa <kyawashell@gmail.com>
2024-05-31 12:53:13 +0900

    Symbolic creator for Windows

---
+++ dotfilesLinkEssential.ps1
@@ -0,0 +1,22 @@
+function Create-SymbolicLinkIfNotExists {
+    param (
+        [string]$SourcePath,
+        [string]$LinkPath
+    )
+
+    # リンクパスが存在するか確認
+    if (-Not (Test-Path -Path $LinkPath)) {
+        # リンクパスが存在しない場合、シンボリックリンクを作成
+        New-Item -ItemType SymbolicLink -Path $LinkPath -Target $SourcePath
+        Write-Output "symbolic link was created."
+    } else {
+        # リンクパスが既に存在する場合
+        Write-Output "There is already file or directory '$LinkPath'."
+    }
+}
+
+Create-SymbolicLinkIfNotExists -SourcePath $HOME\dotfiles\.vimrc -LinkPath $HOME\.vimrc
+Create-SymbolicLinkIfNotExists -SourcePath $HOME\dotfiles\.vim -LinkPath $HOME\.vim
+Create-SymbolicLinkIfNotExists -SourcePath $HOME\dotfiles\.gitconfig -LinkPath $HOME\.gitconfig
+Create-SymbolicLinkIfNotExists -SourcePath $HOME\dotfiles\.config\nvim -LinkPath $env:LOCALAPPDATA\nvim
+Create-SymbolicLinkIfNotExists -SourcePath $HOME\dotfiles\.config\efm-langserver  -LinkPath $env:APPDATA\efm-langserver
---
+++ dotfilesLinkTerminals.ps1
@@ -0,0 +1,31 @@
+# ディレクトリが存在するか確認
+$directoryPath = "$HOME\.config"
+if (-Not (Test-Path -Path $directoryPath)) {
+    # ディレクトリが存在しない場合、作成
+    New-Item -ItemType Directory -Path $directoryPath
+    Write-Output "Config directory was created."
+} else {
+    # ディレクトリが既に存在する場合
+    Write-Output "Config directory is already existed."
+}
+
+function Create-SymbolicLinkIfNotExists {
+    param (
+        [string]$SourcePath,
+        [string]$LinkPath
+    )
+
+    # リンクパスが存在するか確認
+    if (-Not (Test-Path -Path $LinkPath)) {
+        # リンクパスが存在しない場合、シンボリックリンクを作成
+        New-Item -ItemType SymbolicLink -Path $LinkPath -Target $SourcePath
+        Write-Output "symbolic link was created."
+    } else {
+        # リンクパスが既に存在する場合
+        Write-Output "There is already file or directory."
+    }
+}
+
+Create-SymbolicLinkIfNotExists -SourcePath $HOME\dotfiles\.config\wezterm -LinkPath $HOME\.config\wezterm
+Create-SymbolicLinkIfNotExists -SourcePath $HOME\dotfiles\.config\alacritty -LinkPath $HOME\.config\alacritty
+Create-SymbolicLinkIfNotExists -SourcePath $HOME\dotfiles\.config\kitty -LinkPath $HOME\.config\kitty
