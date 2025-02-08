738f030ecf26fe1c109ba4e3464018715a8e1d00
kyawakyawa <kyawashell@gmail.com>
2025-02-08 16:36:27 +0900

    ollama parsers

--- .config/nvim/lua/avante_cfg/init.lua
+++ .config/nvim/lua/avante_cfg/init.lua
@@ -1,21 +1,14 @@
 local avante_cfg = {}
 
 local util = require('util')
-local config = util.load_config()
-avante_config = config['plugins']['aiAssistant']['avante']
-avante_config_opts = config['plugins']['aiAssistant']['avante']['opts']
+local ollama = require('avante_cfg.ollama')
 
 avante_cfg_opts = {
   -- provider = "ollama",
   provider = avante_config_opts["provider"],
   vendors = {
     ---@type AvanteProvider
-    ollama = {
-      __inherited_from = "openai",
-      api_key_name = "",
-      endpoint = avante_config_opts["venders"]["ollama"]["endpoint"],
-      model = avante_config_opts["venders"]["ollama"]["model"],
-    },
+    ollama = ollama,
   },
 }
 
