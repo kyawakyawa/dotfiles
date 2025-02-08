fe29b9a306ee12e9c26d2382902e8bb947f7e094
kyawakyawa <kyawashell@gmail.com>
2025-02-08 14:15:43 +0900

    add avante

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -28,6 +28,11 @@ if config['plugins']['aiAssistant']['githubCopilot']["enabled"] then
   plugins = require('plugins_copilot').setup(plugins)
 end
 
+-- avante.nvim
+if config['plugins']['aiAssistant']['avante']["enabled"] then
+  plugins = require('plugins_avante').setup(plugins)
+end
+
 -- fuzzy finder
 plugins = require('plugins_fuzzy_finder').setup(plugins)
 
---
+++ .config/nvim/lua/plugins_avante.lua
@@ -0,0 +1,73 @@
+local avante = {}
+
+util = require('util')
+
+avante.setup = function(plugins)
+
+  if vim.fn.executable('cargo') == 1 then
+    util.add_plugin(plugins, {
+        "yetone/avante.nvim",
+        event = "VeryLazy",
+        lazy = false,
+        version = false, -- set this to "*" if you want to always pull the latest change, false to update on release
+        opts = {
+          -- provider = "ollama",
+          provider = "copilot",
+          vendors = {
+            ---@type AvanteProvider
+            ollama = {
+              __inherited_from = "openai",
+              api_key_name = "",
+              endpoint = "127.0.0.1:11434/v1",
+              model = "qwen2.5-coder:32b-instruct-q8_0",
+            },
+          },
+        },
+        -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+        build = "make",
+        -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+        dependencies = {
+          "stevearc/dressing.nvim",
+          "nvim-lua/plenary.nvim",
+          "MunifTanjim/nui.nvim",
+          --- The below dependencies are optional,
+          "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
+          "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+          "zbirenbaum/copilot.lua", -- for providers='copilot'
+          {
+            -- support for image pasting
+            "HakonHarnes/img-clip.nvim",
+            event = "VeryLazy",
+            opts = {
+              -- recommended settings
+              default = {
+                embed_image_as_base64 = false,
+                prompt_for_file_name = false,
+                drag_and_drop = {
+                  insert_mode = true,
+                },
+                -- required for Windows users
+                use_absolute_path = true,
+              },
+            },
+          },
+          {
+            -- Make sure to set this up properly if you have lazy=true
+            'MeanderingProgrammer/render-markdown.nvim',
+            opts = {
+              file_types = { "markdown", "Avante" },
+            },
+            ft = { "markdown", "Avante" },
+          },
+        },
+      },
+      {
+        vscode = false
+      }
+    )
+  end
+
+  return plugins
+end
+
+return avante
