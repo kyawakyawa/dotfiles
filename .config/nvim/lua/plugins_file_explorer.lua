6bff2d4250a78cb4840f9d29cde673429c9d9fd8
kyawakyawa <kyawashell@gmail.com>
2025-05-20 17:31:10 +0900

    diagnostic config

--- .config/nvim/lua/plugins_file_explorer.lua
+++ .config/nvim/lua/plugins_file_explorer.lua
@@ -17,15 +17,6 @@ file_explorer.setup = function(plugins)
       { "<space>g", "<cmd>Neotree toggle source=git_status reveal=true position=float <cr>", desc = "NeoTree git" },
     },
     config = function ()
-      vim.fn.sign_define("DiagnosticSignError",
-        {text = " ", texthl = "DiagnosticSignError"})
-      vim.fn.sign_define("DiagnosticSignWarn",
-        {text = " ", texthl = "DiagnosticSignWarn"})
-      vim.fn.sign_define("DiagnosticSignInfo",
-        {text = " ", texthl = "DiagnosticSignInfo"})
-      vim.fn.sign_define("DiagnosticSignHint",
-        {text = "󰌵", texthl = "DiagnosticSignHint"})
-
       require("neo-tree").setup({
         default_component_configs = {
           window = {
