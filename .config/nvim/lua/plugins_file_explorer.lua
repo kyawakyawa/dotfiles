629fb8a1aac5fbd6ab64c20999cb2298c556e53d
kyawakyawa <kyawashell@gmail.com>
2025-07-14 16:57:44 +0900

    update

--- .config/nvim/lua/plugins_file_explorer.lua
+++ .config/nvim/lua/plugins_file_explorer.lua
@@ -12,15 +12,15 @@ file_explorer.setup = function(plugins)
     -- TODO config
     lazy = true,
     keys = {
-      { "<space>e", "<cmd>Neotree toggle source=filesystem reveal=true position=float<cr>", desc = "NeoTree" },
-      -- { "<space>b", "<cmd>Neotree toggle source=buffer reveal=true position=float<cr>", desc = "NeoTree git" },
-      { "<space>g", "<cmd>Neotree toggle source=git_status reveal=true position=float <cr>", desc = "NeoTree git" },
+      { "<space>e", "<cmd>Neotree toggle source=filesystem reveal=true position=left<cr>", desc = "NeoTree" },
+      -- { "<space>b", "<cmd>Neotree toggle source=buffer reveal=true position=float<cr>", desc = "NeoTree buf" },
+      { "<space>g", "<cmd>Neotree toggle source=git_status reveal=true position=left <cr>", desc = "NeoTree git" },
     },
     config = function ()
       require("neo-tree").setup({
         default_component_configs = {
           window = {
-            position = "float",
+            -- position = "float",
           }
         }
       })
