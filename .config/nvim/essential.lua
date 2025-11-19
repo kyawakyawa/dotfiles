d1df491bc4c6a2af4c42de180e0538c5e4cb939f
kyawakyawa <kyawashell@gmail.com>
2025-11-19 12:16:41 +0900

    setting for pyenv env

--- .config/nvim/essential.lua
+++ .config/nvim/essential.lua
@@ -13,3 +13,13 @@ vim.cmd([[
   set hidden
   colorscheme vim
 ]])
+
+-- Python provider 完全オフ
+vim.g.loaded_python3_provider = 0
+-- もしPython providerを使う場合は以下を書く(Pyenv環境だとshimが遅くて起動が遅れるので使うPythonを明示的に指定する)
+-- vim.g.python3_host_prog = '/usr/bin/python3'
+
+-- その他使わないproviderもオフにする
+vim.g.loaded_node_provider = 0
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_ruby_provider = 0
