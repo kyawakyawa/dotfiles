5b889df9f88a62221dcfd82168c315c3a949b9c0
kyawakyawa <kyawashell@gmail.com>
2025-12-12 15:58:39 +0900

    update download_cli_tools.sh

--- download_cli_tools.sh
+++ download_cli_tools.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install
-# delta peco ripgrep ghq git-graph lazygit yazi
+# delta peco ripgrep ghq git-graph lazygit yazi fd
 
 OUTDIR=${HOME}/bin/
 BASH_CMP_D=${HOME}/.config/bash_completion.d
@@ -30,6 +30,8 @@ LAZYGIT_VERSION=0.57.0
 YAZI_LINUX_ARCH=x86_64 # aarch64
 YAZI_VERSION=v25.5.31
 
+FD_LINUX_ARCH=x86_64 # aarch64
+FD_VERSION=v10.3.0
 
 DELTA_URL=""
 PECO_URL=""
@@ -37,6 +39,7 @@ RG_URL=""
 GHQ_URL=""
 LAZYGIT_URL=""
 YAZI_URL=""
+FD_URL=""
 
 if [ "$(uname)" == 'Darwin' ]; then
   DELTA_URL=https://github.com/dandavison/delta/releases/download/${DELTA_VERSION}/delta-${DELTA_VERSION}-aarch64-apple-darwin.tar.gz
@@ -45,6 +48,7 @@ if [ "$(uname)" == 'Darwin' ]; then
   GHQ_URL=https://github.com/x-motemen/ghq/releases/download/${GHQ_VERSION}/ghq_darwin_arm64.zip
   LAZYGIT_URL="https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_darwin_arm64.tar.gz"
   YAZI_URL="https://github.com/sxyazi/yazi/releases/download/${YAZI_VERSION}/yazi-aarch64-apple-darwin.zip"
+  FD_URL="https://github.com/sharkdp/fd/releases/download/${FD_VERSION}/fd-v10.3.0-aarch64-apple-darwin.tar.gz"
 elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
 
   DELTA_URL=https://github.com/dandavison/delta/releases/download/${DELTA_VERSION}/delta-${DELTA_VERSION}-${DELTA_LINUX_ARCH}-unknown-linux-musl.tar.gz
@@ -53,6 +57,7 @@ elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
   GHQ_URL=https://github.com/x-motemen/ghq/releases/download/${GHQ_VERSION}/ghq_linux_${GHQ_LINUX_ARCH}.zip
   LAZYGIT_URL="https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_linux_${LAZYGIT_LINUX_ARCH}.tar.gz"
   YAZI_URL="https://github.com/sxyazi/yazi/releases/download/${YAZI_VERSION}/yazi-${YAZI_LINUX_ARCH}-unknown-linux-musl.zip"
+  FD_URL="https://github.com/sharkdp/fd/releases/download/${FD_VERSION}/fd-${FD_VERSION}-${FD_LINUX_ARCH}-unknown-linux-musl.tar.gz"
 else
   echo "Your platform ($(uname -a)) is not supported."
   exit 1
@@ -97,6 +102,12 @@ cp $(basename $YAZI_URL .zip)/ya $OUTDIR/
 cp $(basename $YAZI_URL .zip)/yazi $OUTDIR/
 cp $(basename $YAZI_URL .zip)/completions/ya.bash $BASH_CMP_D/
 cp $(basename $YAZI_URL .zip)/completions/yazi.bash $BASH_CMP_D/
+
+# FD
+curl -LO $FD_URL 
+tar zxvf $(basename $FD_URL)
+cp $(basename $FD_URL .tar.gz)/fd $OUTDIR/
+cp $(basename $FD_URL .tar.gz)/autocomplete/fd.bash $BASH_CMP_D/
 
 # Finalize
 cd .. && rm -r $TMPDIR
