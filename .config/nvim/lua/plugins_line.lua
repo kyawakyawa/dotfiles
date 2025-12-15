2ac819391cd72105c53ebf556094258acdf77992
kyawakyawa <kyawashell@gmail.com>
2025-12-15 17:00:34 +0900

    update incline.nvim

--- .config/nvim/lua/plugins_line.lua
+++ .config/nvim/lua/plugins_line.lua
@@ -2,17 +2,17 @@ local line = {}
 local util = require("util")
 
 local setup_incline = function()
-  local devicons = require 'nvim-web-devicons'
-  require('incline').setup {
+  local devicons = require("nvim-web-devicons")
+  require("incline").setup({
     render = function(props)
-      local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ':t')
-      if filename == '' then
-        filename = '[No Name]'
+      local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
+      if filename == "" then
+        filename = "[No Name]"
       end
       local ft_icon, ft_color = devicons.get_icon_color(filename)
 
       local function get_git_diff()
-        local icons = { removed = '', changed = '', added = '' }
+        local icons = { removed = "", changed = "", added = "" }
         local signs = vim.b[props.buf].gitsigns_status_dict
         local labels = {}
         if signs == nil then
@@ -20,27 +20,30 @@ local setup_incline = function()
         end
         for name, icon in pairs(icons) do
           if tonumber(signs[name]) and signs[name] > 0 then
-            table.insert(labels, { icon .. signs[name] .. ' ', group = 'Diff' .. name })
+            table.insert(labels, { icon .. signs[name] .. " ", group = "Diff" .. name })
           end
         end
         if #labels > 0 then
-          table.insert(labels, { '┊ ' })
+          table.insert(labels, { "┊ " })
         end
         return labels
       end
 
       local function get_diagnostic_label()
-        local icons = { error = '', warn = '', info = '', hint = '' }
+        local icons = { error = "", warn = "", info = "", hint = "" }
         local label = {}
 
         for severity, icon in pairs(icons) do
-          local n = #vim.diagnostic.get(props.buf, { severity = vim.diagnostic.severity[string.upper(severity)] })
+          local n = #vim.diagnostic.get(
+            props.buf,
+            { severity = vim.diagnostic.severity[string.upper(severity)] }
+          )
           if n > 0 then
-            table.insert(label, { icon .. n .. ' ', group = 'DiagnosticSign' .. severity })
+            table.insert(label, { icon .. n .. " ", group = "DiagnosticSign" .. severity })
           end
         end
         if #label > 0 then
-          table.insert(label, { '┊ ' })
+          table.insert(label, { "┊ " })
         end
         return label
       end
@@ -51,13 +54,13 @@ local setup_incline = function()
       return {
         { get_diagnostic_label() },
         { get_git_diff() },
-        { (ft_icon or '') .. ' ', guifg = ft_color, guibg = 'none' },
-        { filename .. ' ', gui = vim.bo[props.buf].modified and 'bold,italic' or 'bold' },
+        { (ft_icon or "") .. " ", guifg = ft_color, guibg = "none" },
+        { filename .. " ", gui = vim.bo[props.buf].modified and "bold,italic" or "bold" },
         -- { '┊  ' .. vim.api.nvim_win_get_number(props.win), group = 'DevIconWindows' },
-        { modified and { ' ', guifg = '#888888', gui = 'bold' } or '', }
+        { modified and { " ", guifg = "#ff0000", gui = "bold" } or "" },
       }
     end,
-  }
+  })
 end
 
 line.setup = function(plugins)
@@ -68,9 +71,9 @@ line.setup = function(plugins)
       setup_incline()
     end,
     -- Optional: Lazy load Incline
-    event = 'VeryLazy',
+    event = "VeryLazy",
     dependencies = {
-      "lewis6991/gitsigns.nvim",     -- OPTIONAL: for git status
+      "lewis6991/gitsigns.nvim", -- OPTIONAL: for git status
       "nvim-tree/nvim-web-devicons", -- OPTIONAL: for file icons
     },
   })
