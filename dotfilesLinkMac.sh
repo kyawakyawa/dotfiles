3ff3ba1339c90adcf418b2162ce65e3e77f4cffa
kyawakyawa <kyawashell@gmail.com>
2026-01-25 17:07:31 +0900

    add .zprofile and .zshrc for mac

---
+++ dotfilesLinkMac.sh
@@ -0,0 +1,2 @@
+if [ ! -e ~/.zprofile ]; then ln -s ~/dotfiles/.zprofile_for_mac ~/.zprofile ; else echo .zprofile is exist; fi
+if [ ! -e ~/.zshrc ]; then ln -s ~/dotfiles/.zshrc_for_mac ~/.zshrc ; else echo .zprofile is exist; fi
