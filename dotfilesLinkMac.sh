be5e6e0e7fb34b0c36b53e29dbe68bfdd6cf588f
kyawakyawa <kyawashell@gmail.com>
2026-02-01 09:26:28 +0900

    Resolve dotfiles path from script location

--- dotfilesLinkMac.sh
+++ dotfilesLinkMac.sh
@@ -1,2 +1,5 @@
-if [ ! -e ~/.zprofile ]; then ln -s ~/dotfiles/.zprofile_for_mac ~/.zprofile ; else echo .zprofile is exist; fi
-if [ ! -e ~/.zshrc ]; then ln -s ~/dotfiles/.zshrc_for_mac ~/.zshrc ; else echo .zprofile is exist; fi
+#! /bin/bash
+DOTFILES_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+if [ ! -e ~/.zprofile ]; then ln -s "$DOTFILES_DIR/.zprofile_for_mac" ~/.zprofile ; else echo .zprofile is exist; fi
+if [ ! -e ~/.zshrc ]; then ln -s "$DOTFILES_DIR/.zshrc_for_mac" ~/.zshrc ; else echo .zprofile is exist; fi
